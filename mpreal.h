894af5e4874fd7ff1826bcaa789c8762f2cd2b0e
Pavel Holoborodko <pavel@holoborodko.com>
2020-10-30 12:04:09 +0900

    Bumped version to 3.6.8

--- mpreal.h
+++ mpreal.h
@@ -69,8 +69,8 @@
 // Library version
 #define MPREAL_VERSION_MAJOR 3
 #define MPREAL_VERSION_MINOR 6
-#define MPREAL_VERSION_PATCHLEVEL 7
-#define MPREAL_VERSION_STRING "3.6.7"
+#define MPREAL_VERSION_PATCHLEVEL 8
+#define MPREAL_VERSION_STRING "3.6.8"
 
 // Detect compiler using signatures from http://predef.sourceforge.net/
 #if defined(__GNUC__) && defined(__INTEL_COMPILER)
